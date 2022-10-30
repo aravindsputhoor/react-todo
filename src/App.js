3adc814198a3d6feba8a38ea38908d589359d134
Aravind S <aravindsputhoor@gmail.com>
2022-10-30 22:29:02 +0530

    add todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import './App.css';
 import List from './components/List';
+import {useState} from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
   return (
     <div>
       <div className="header">
       <div className="p-4">
         <h3>Todo planner</h3>
         <div className="pseudo-search">
-          <input className="mt-2" type="text" placeholder="Enter todo here.." />
-          <button className="fa fa-plus" type="button"></button>
+          <input value={todo} onChange={(e) => setTodo(e.target.value)} className="mt-2 white" type="text" placeholder="Enter todo here.." />
+          <button className="fa fa-plus white" onClick={() => setTodos(() => { return [...todos, {todoText: todo, time: Date.now(), status: 'todo'}]})} type="button"></button>
         </div>
       </div>
     </div>
 
     <div className="container mt-2">
       <div className="row">
-        <List header={'todo'} />
-        <List header={'completed'} />
-        <List header={'removed'} />
+        <List status={'todo'} data={todos} />
+        <List status={'completed'} data={todos} />
+        <List status={'removed'} data={todos} />
       </div>
     </div>
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -5,21 +5,27 @@ const List = (props) => {
   return (
     <div className="col-sm-4">
       <ul className="list-group">
-        {props.header == 'todo' ? <li className="list-group-item blue-head">Todo's</li> : props.header == 'completed' ? <li className="list-group-item green-head">Completed</li> : <li className="list-group-item red-head">Removed</li>}
-        <li className="list-group-item">
-          {props.header == 'todo' ?
-            <div class="row">
-              <div className="col-sm-8">
-                <p>Dapibus ac facilisis insxasdasdasdasdasd sadasdasd scasd</p>
-              </div>
-              <div className="col-sm-4">
-                <button className="btn btn-success btn-circle btn-circle-sm m-1"><i className="fa fa-check"></i></button>
-                <button className="btn btn-danger btn-circle btn-circle-sm m-1"><i className="fa fa-trash"></i></button>
-              </div>
-            </div> : <p>Porta ac consectetur ac</p>
+        {props.status == 'todo' ? <li className="list-group-item blue-head">Todo's</li> : props.status == 'completed' ? <li className="list-group-item green-head">Completed</li> : <li className="list-group-item red-head">Removed</li>}
+       
+          { props.data.length == 0 ? <li className="list-group-item center"><p><b>No Todo's</b></p></li>  : props.data.map((value) => {
+            if (props.status == value.status) {
+              return <li className="list-group-item">
+                <div className="row">
+                  <div className="col-sm-8">
+                    <p>{value.todoText}</p>
+                  </div>
+                  <div className="col-sm-4 center">
+                    <button className="btn btn-success btn-circle btn-circle-sm m-1"><i className="fa fa-check"></i></button>
+                    <button className="btn btn-danger btn-circle btn-circle-sm m-1"><i className="fa fa-trash"></i></button>
+                  </div>
+                </div>
+                
+                <a>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(value.time)}</a>
+              </li>
+            }
+          })
           }
-          <a>sdcadcasd</a>
-        </li>
+        
       </ul>
     </div>
   )

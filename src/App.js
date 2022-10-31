16808b5e371e90e64b115536e98b6bde300953f9
Aravind S <aravindsputhoor@gmail.com>
2022-10-31 12:14:58 +0530

    defects fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import './App.css';
 import List from './components/List';
 import {useState} from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState('');
+  const addTodo = () => {
+    let bool = false;
+    console.log('sx');
+    todos.map((value, key)=>{
+      if(value.todoText == todo) {
+        bool = true;
+      } 
+    })
+    if(bool){
+      toast.warn("Todo already exists!");
+      return [...todos]
+    } else {
+      return [...todos, {todoText: todo, time: Date.now(), status: 'todo'}]
+    }
+  }
 
   return (
     <div>
+      <ToastContainer />
       <div className="header">
       <div className="p-4">
         <h3>Todo planner</h3>
         <div className="pseudo-search">
           <input value={todo} onChange={(e) => setTodo(e.target.value)} className="mt-2 white" type="text" placeholder="Enter todo here.." />
-          <button className="fa fa-plus white" onClick={() => setTodos(() => { return [...todos, {todoText: todo, time: Date.now(), status: 'todo'}]})} type="button"></button>
+          <button className="fa fa-plus white" onClick={() => setTodos(addTodo)} type="button"></button>
         </div>
       </div>
     </div>
 
     <div className="container mt-2">
       <div className="row">
-        <List status={'todo'} data={todos} />
-        <List status={'completed'} data={todos} />
-        <List status={'removed'} data={todos} />
+        <List status={'todo'} data={todos} function= {setTodos}/>
+        <List status={'completed'} data={todos} function= {setTodos}/>
+        <List status={'removed'} data={todos} function= {setTodos}/>
       </div>
     </div>
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 const List = (props) => {
+  let todos = props.data;
+  let listData = props.data.filter((value)=>{
+    return value.status === props.status;
+  })
 
   return (
     <div className="col-sm-4">
       <ul className="list-group mt-4">
-        {props.status == 'todo' ? <li className="list-group-item blue-head"><i className="fa fa-cog" aria-hidden="true"></i> Todo's</li> : props.status == 'completed' ? <li className="list-group-item green-head"><i className="fa fa-cog" aria-hidden="true"></i> Completed</li> : <li className="list-group-item red-head"><i className="fa fa-cog" aria-hidden="true"></i> Removed</li>}
+        {props.status === 'todo' ? <li className="list-group-item blue-head"><i className="fa fa-cog" aria-hidden="true"></i> Todo's</li> : props.status === 'completed' ? <li className="list-group-item green-head"><i className="fa fa-cog" aria-hidden="true"></i> Completed</li> : <li className="list-group-item red-head"><i className="fa fa-cog" aria-hidden="true"></i> Removed</li>}
        
-          { props.data.length == 0 ? <li className="list-group-item center"><p><b>No Todo's</b></p></li>  : props.data.map((value) => {
-            if (props.status == value.status) {
-              return <li className="list-group-item">
+          { (listData.length === 0) ? <li className="list-group-item center"><p><b>No Todo's</b></p></li>  : listData.map((value, key) => {
+            if (props.status === value.status) {
+              return props.status==='todo' ? <li className="list-group-item" key={key}>
                 <div className="row d-flex">
                   <div className="col-sm-8">
                     <p><b>{value.todoText}</b></p>
                     <a>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(value.time)}</a>
                   </div>
                   <div className="col-sm-4 end">
-                    <button className="btn btn-success btn-circle btn-circle-sm m-1"><i className="fa fa-check"></i></button>
-                    <button className="btn btn-danger btn-circle btn-circle-sm m-1"><i className="fa fa-trash"></i></button>
+                    <button className="btn btn-success btn-circle btn-circle-sm m-1" onClick={() => {}} ><i className="fa fa-check"></i></button>
+                    <button className="btn btn-danger btn-circle btn-circle-sm m-1" onClick={() => {}} ><i className="fa fa-trash"></i></button>
                   </div>
                 </div>
+              </li> : 
+              <li className="list-group-item" key={key}>
+                <p><b>{value.todoText}</b></p>
+                <a>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(value.time)}</a>
               </li>
             }
           })
